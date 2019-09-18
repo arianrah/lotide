3150a8edb223ec79e4ac9e5f59c8454403f2edf4
Arian Rahimi <arianrahimi@icloud.com>
2019-09-18 00:33:50 +0000

    without function completed

--- without.js
+++ without.js
@@ -1,26 +1,39 @@
-import baseDifference from './.internal/baseDifference.js'
-import isArrayLikeObject from './isArrayLikeObject.js'
-
-/**
- * Creates an array excluding all given values using
- * [`SameValueZero`](http://ecma-international.org/ecma-262/7.0/#sec-samevaluezero)
- * for equality comparisons.
- *
- * **Note:** Unlike `pull`, this method returns a new array.
- *
- * @since 0.1.0
- * @category Array
- * @param {Array} array The array to inspect.
- * @param {...*} [values] The values to exclude.
- * @returns {Array} Returns the new array of filtered values.
- * @see difference, union, unionBy, unionWith, xor, xorBy, xorWith
- * @example
- *
- * without([2, 1, 2, 3], 1, 2)
- * // => [3]
- */
-function without(array, ...values) {
-  return isArrayLikeObject(array) ? baseDifference(array, values) : []
+const eqArrays = function(firstArr, secondArr) {
+  let newArr = [];
+  if (firstArr.length !== secondArr.length) {
+    return false;
+  } else {
+    for (let i = 0; i < firstArr.length; i++){
+      if (firstArr[i] === secondArr[i]){
+        newArr.push(firstArr[i]);
+      } else {
+        newArr.pop();
+      }
+    }
+  }
+  if (newArr.length === firstArr.length){
+    return true
+  } else {
+    return false
+  }
 }
 
-export default without
+const assertArraysEquals = function(firstArr, secondArr) {
+  if (eqArrays(firstArr, secondArr) === true){
+    console.log(`✅✅✅ Assertion Passed`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed`)
+  }
+}
+
+const without = function(source, itemToRemove) {
+  let newArr = [];
+  for (let i = 0; i < source.length; i++){
+    if (source[i] !== itemToRemove[i]){
+      newArr.push(source[i]);
+    }
+  }
+  return newArr;
+}
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
